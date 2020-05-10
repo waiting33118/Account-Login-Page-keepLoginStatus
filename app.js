9df59873e3bc1087ffca6d798921763264ef7dc3
Tony <waiting33118@gmail.com>
2020-05-10 23:16:41 +0800

    add session params & set logout router

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphdbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
 const checkPermission = require('./permission-checker')
 const app = express()
 const hostname = '127.0.0.1'
@@ -11,12 +12,34 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
+//express-session params
+app.use(
+	session({
+		secret: 'secret string for login validation',
+		resave: false,
+		saveUninitialized: true,
+		cookie: {
+			secure: false,
+		},
+	})
+)
+
 app.get('/', (req, res) => {
-	res.render('home')
+	if (!req.session.info) {
+		res.render('home')
+	} else {
+		res.render('userPage', { checkedResult: req.session.info })
+	}
+})
+
+app.get('/logout', (req, res) => {
+	req.session.destroy()
+	res.redirect('/')
 })
 
 app.post('/', (req, res) => {
 	const checkedResult = checkPermission(req.body)
+	req.session.info = checkedResult
 	if (!checkedResult) {
 		const userInput = req.body
 		const errMessage = 'Wrong Email or Password!'
